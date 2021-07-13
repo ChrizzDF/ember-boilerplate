bfb1e9520a4c9c944654286c4279df8eafdbc7e5
chrizzdf <tools.christoph.langhof@gmail.com>
2021-07-13 08:05:28 +0200

    use TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  mode: 'jit',
+  purge: ['app/**/*.{js,ts,hbs}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
